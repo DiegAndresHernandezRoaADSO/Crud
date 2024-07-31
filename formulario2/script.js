e26ebef125129973c5d035d16a59ad42eb7c1ce1
Diego Hernandez <hernandezroadiegoandres@gmail.com>
2024-07-31 16:24:40 -0500

    mi crud desde casa

--- formulario2/script.js
+++ formulario2/script.js
@@ -1,7 +1,19 @@
-function añardir_elemento(){
-    let aficiones = document.getElementById("tipo");
-    let inputAficion = document.getElementById("inputAficion").value;
-    let option = document.createElement("option");
-    option.text = inputAficion;
-    aficiones.add(option);
+
+const $formulario = document.querySelector('form');
+const nombre = document.querySelector("#nombre");
+
+$formulario.addEventListener("submit", (event)=>{
+    let response = isvalid(event, "from[required]");
+    const data ={
+        first_name: nombre.value,
     }
+    if (response) {
+        fetch('http://localhost:3000/user',{
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    });
+    }
+})
